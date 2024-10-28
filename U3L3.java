ee9a54d28a284e6ea7556209e5991c8a09a01669
BT_3W2_26 <khaledo@nycstudents.net>
2024-10-28 13:19:02 -0400

    ur mum

---
+++ U3L3.java
@@ -0,0 +1,15 @@
+public class U3L3 {
+    public static void main(String[] args) {
+        int p = 5;
+        int q = 0;
+
+        if (p > 0 && (p / q) == 2)
+        {
+            System.out.println("it’s true :)");
+        }
+        else
+        {
+            System.out.println("it’s false :(");
+        }
+    }
+}
